1aaf5aa387b44df624ef7c74340ebc583c5bd681
David Fung <davidfung@amgcomputing.com>
2024-05-31 13:45:24 -0700

    Add cli option prefix & sufix

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ var year int
 var dow string
 var subfolder string
 var format string
+var prefix string
+var suffix string
 
 func init() {
 	current_year := time.Now().Year()
@@ -18,6 +20,8 @@ func init() {
 	flag.StringVar(&dow, "dow", "", "Comma-delimited day of weeks (Mon,Tue,Wed,Thu,Fri,Sat,Sun)")
 	flag.StringVar(&subfolder, "subfolder", "", "Create subfolder")
 	flag.StringVar(&format, "format", "YYYY-MM-DD", "Date format, default YYYY-MM-DD")
+	flag.StringVar(&prefix, "prefix", "", "Prefix add to folder name")
+	flag.StringVar(&suffix, "suffix", "", "Suffix add to folder name")
 }
 
 func main() {
@@ -26,6 +30,8 @@ func main() {
 	for _, v := range strings.Split(dow, ",") {
 		fmt.Printf("dow = %s\n", v)
 	}
-	fmt.Printf("subfolder = %s\n", subfolder)
-	fmt.Printf("format = %s\n", format)
+	fmt.Printf("subfolder = %q\n", subfolder)
+	fmt.Printf("format = %q\n", format)
+	fmt.Printf("prefix = %q\n", prefix)
+	fmt.Printf("suffix = %q\n", suffix)
 }
