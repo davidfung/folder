9e58cea97485638724a6b0bd17b5b4391ad294cf
David Fung <davidfung@amgcomputing.com>
2024-06-15 01:32:03 -0700

    Add cli option: scan, template, base

--- main.go
+++ main.go
@@ -13,6 +13,9 @@ var subfolder string
 var format string
 var prefix string
 var suffix string
+var scan string
+var template string
+var base string
 
 func init() {
 	current_year := time.Now().Year()
@@ -22,10 +25,17 @@ func init() {
 	flag.StringVar(&format, "format", "YYYY-MM-DD", "Date format, default YYYY-MM-DD")
 	flag.StringVar(&prefix, "prefix", "", "Prefix add to folder name")
 	flag.StringVar(&suffix, "suffix", "", "Suffix add to folder name")
+	flag.StringVar(&scan, "scan", "", "Base folder to scan folder tree")
+	flag.StringVar(&template, "template", "", "Create folder tree based on template")
+	flag.StringVar(&base, "base", "", "Base folder")
 }
 
 func main() {
 	flag.Parse()
+	checkUsage()
+}
+
+func checkUsage() {
 	fmt.Printf("year = %d\n", year)
 	for _, v := range strings.Split(dow, ",") {
 		fmt.Printf("dow = %s\n", v)
@@ -34,4 +44,7 @@ func main() {
 	fmt.Printf("format = %q\n", format)
 	fmt.Printf("prefix = %q\n", prefix)
 	fmt.Printf("suffix = %q\n", suffix)
+	fmt.Printf("scan = %q\n", scan)
+	fmt.Printf("template = %q\n", template)
+	fmt.Printf("base = %q\n", base)
 }
